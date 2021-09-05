e41a69529b361cc0ed9244172eb4d3e5a377ecec
hannah-rae <hannah.r.kerner@gmail.com>
2021-09-05 11:48:34 -0400

    Add option to plot in terms of precision at N. #53

--- dora_exp_pipeline/combined_plot_script.py
+++ dora_exp_pipeline/combined_plot_script.py
@@ -44,7 +44,7 @@ def validation_labels(filename):
     return labels
 
 
-def get_curve(scores, validationLabels):
+def get_selections_curve(scores, validationLabels):
     x = list(range(1, len(scores)+1))
     y = []
     numOutliers = 0
@@ -57,36 +57,61 @@ def get_curve(scores, validationLabels):
     return x, y
 
 
-def combine_plots(resultsdir, label_path):
+def get_precision_curve(scores, validationLabels):
+    x = list(range(1, len(scores)+1))
+    y = []
+    numOutliers = 0
+    for i in range(len(scores)):
+        if(validationLabels[scores[i]] == 1):
+            numOutliers += 1
+        y.append(float(numOutliers)/(i+1))
+
+    return x, y
+
+
+def combine_plots(resultsdir, label_path, precision_at_n):
     filenames, labels = filenames_and_labels(resultsdir)
 
-    maxX, maxY = -1, -1
     fig, axes = plt.subplots()
 
     validationLabels = validation_labels(label_path)
+    num_outliers = sum([x for x in validationLabels.values()])
+
+    if precision_at_n:
+        plt.axhline(y=1, label='Best line', linestyle='--', color='k')
+        plt.title('Precision at N')
+        plt.xlabel('Number of selections (N)')
+        plt.ylabel('Precision at N')
+    else:
+        bestLine = list(range(1, num_outliers+1))
+        plt.plot(bestLine, bestLine, label='Best line', linestyle='--',
+                 color='k')
+        plt.title('True Outliers vs. Algorithm Selections')
+        plt.xlabel('Number of selections')
+        plt.ylabel('Number of true outliers')
+
     for i in range(len(filenames)):
         scores = alg_indexes(filenames[i])
 
-        x, y = get_curve(scores, validationLabels)
-        maxX = max(maxX, x[-1])
-        maxY = max(maxY, y[-1])
+        if precision_at_n:
+            x, y = get_precision_curve(scores, validationLabels)
+            plt.plot(x, y, label="{}".format(labels[i]))
+        else:
+            x, y = get_selections_curve(scores, validationLabels)
+            index = x.index(y[-1])
+            area = np.trapz(y[:index+1], x[:index+1])
+            plt.plot(x, y, label="{} (AUC: {})".format(labels[i], area))
 
-        index = x.index(y[-1])
-        # area = np.trapz(y, x)
-        area = np.trapz(y[:index+1], x[:index+1])
-
-        plt.plot(x, y, label="{} (AUC: {})".format(labels[i], area))
-
-    bestLine = list(range(1, maxX+1))
-    plt.plot(bestLine, bestLine, label='Best Line', linestyle='--', color='k')
-    plt.title('True Outliers vs. Algorithm Selections')
-    plt.xlabel('Number of selections')
-    plt.ylabel('Number of true outliers')
-    plt.legend(loc='upper left')
-    axes.set_xlim(1, maxX)
-    axes.set_ylim(1, maxY)
-    plt.savefig(f'{resultsdir}/comparison_plot_combined.png')
-    plt.savefig(f'{resultsdir}/comparison_plot_combined.pdf')
+    axes.set_xlim(1, len(scores))
+    plt.legend()
+    if precision_at_n:
+        axes.set_ylim(0, 1)
+        plt.savefig(f'{resultsdir}/comparison_plot_combined_p-at-n.png')
+        plt.savefig(f'{resultsdir}/comparison_plot_combined_p-at-n.pdf')
+    else:
+        axes.set_ylim(1, num_outliers)
+        plt.savefig(f'{resultsdir}/comparison_plot_combined.png')
+        plt.savefig(f'{resultsdir}/comparison_plot_combined.pdf')
 
 
 def main():
@@ -95,6 +120,8 @@ def main():
                         help='Path to the results directory')
     parser.add_argument('-l', '--label_path', type=str,
                         help='Path to the evaluation labels file')
+    parser.add_argument('--precision_at_n', action='store_true',
+                        help='Plot in terms of precision at N')
 
     args = parser.parse_args()
     combine_plots(**vars(args))
